67e8878c1e844c6141dee776c1aa53c714379997
Brandon Vu <61858661+Vufoo@users.noreply.github.com>
2022-10-06 10:48:37 -0500

    Login Authentication and style

--- web-app/frontend/src/App.js
+++ web-app/frontend/src/App.js
@@ -1,35 +1,70 @@
-import React from "react"; 
-import Projects from "./Projects"
-import Login from "./Login"
+import { BrowserRouter, Route, Routes, Link,  } from 'react-router-dom'
+import { useState, } from 'react'
 
+import Login from './Components/Login'
+import Projects from './Components/Projects'
+import Header from './Components/Header'
+import useToken from './Components/useToken'
+import './App.css'
 
-class App extends React.Component {
-  constructor(props) {
-    // in React, we always need to call the superclass constructor first with super() 
-    // if we override the component's constructor
-    super(props)
+function App() {
+  
+  const { token, removeToken, setToken } = useToken();
+  
+  return (
+    
+    <BrowserRouter>
+    <div className="App">
+    <Header token={removeToken}/>
+    
+    {!token && token!=="" && token!== undefined?  
+        <Login setToken={setToken} />
+        :(
+          <>
 
-    // app owns the login state because it needs it in order to do conditional rendering.
-    // shared state should always be as low as possible in the component hierarchy.
-    this.state = { isLoggedIn: false }
-  }
+          <Routes>
 
-  handleLogin = (loggedIn) => {
-    this.setState({
-      isLoggedIn: loggedIn
-    })
-  }
-
-  render() {
-    // conditional rendering
-    if (this.state.isLoggedIn) {
-      return (<Projects />)
-    }
-    else {
-      // Login needs to be able to mutate the login state, so we pass it handleLogin as a prop
-      return (<Login handleLogin={this.handleLogin}/>)
-    }
-  }
+          <Route exact path = "/" element = {<Login setToken={setToken} />}></Route>
+          <Route path = "/projects" element ={<Projects token={token} setToken={setToken}/>}> </Route>
+          </Routes>
+      
+          </>
+          
+        )} 
+    </div>
+    </BrowserRouter>
+    
+  );
 }
+
+// class App extends React.Component {
+  
+//   constructor(props) {
+//     // in React, we always need to call the superclass constructor first with super() 
+//     // if we override the component's constructor
+//     super(props)
+
+//     // app owns the login state because it needs it in order to do conditional rendering.
+//     // shared state should always be as low as possible in the component hierarchy.
+//     this.state = { isLoggedIn: false }
+//   }
+
+//   handleLogin = (loggedIn) => {
+//     this.setState({
+//       isLoggedIn: loggedIn
+//     })
+//   }
+
+//   render() {
+//     // conditional rendering
+//     if (this.state.isLoggedIn) {
+//       return (<Projects />)
+//     }
+//     else {
+//       // Login needs to be able to mutate the login state, so we pass it handleLogin as a prop
+//       return (<Login handleLogin={this.handleLogin}/>)
+//     }
+//   }
+// }
 
 export default App;
