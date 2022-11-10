1e0502a7af1ecfc25749e7d663014983430fc193
Brandon Vu <61858661+Vufoo@users.noreply.github.com>
2022-11-10 13:00:51 -0600

    Adding Header

--- web-app/frontend/src/Components/Header.js
+++ web-app/frontend/src/Components/Header.js
@@ -2,39 +2,15 @@ import axios from "axios";
 import { useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
+
 function Header(props) {
   const navigate = useNavigate();
-  function logMeOut() {
-    axios({
-      method: "POST",
-      url:"/logout",
-    })
-    .then((response) => {
-       props.token()
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-        }
-    })
-    navigate("/")
-    }
-
-
-    function Signup(){
-
-    }
 
     return(
         <header className="App-header">
-           
-            <button onClick={logMeOut}> 
-                Logout
-            </button>
-            <button onClick={Signup}> 
-                Signup
-            </button>
+          Jimbo and Friends EE461L Website
+          
+            
         </header>
     )
 }
