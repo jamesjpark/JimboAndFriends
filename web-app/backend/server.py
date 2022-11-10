de3dc0209abc4602643d2d67526154ccf6712f40
James Park <jamesjpark3163@gmail.com>
2022-11-10 14:54:03 -0600

    api for hwset

--- web-app/backend/server.py
+++ web-app/backend/server.py
@@ -1,10 +1,11 @@
 import json
 from flask import Flask, request, jsonify
 from datetime import datetime, timedelta, timezone
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from pymongo import MongoClient
 import certifi
 from passlib.hash import sha256_crypt
+from bson.json_util import dumps
 
 app = Flask(__name__,static_folder= './build', static_url_path='/')
 CORS(app)
@@ -19,114 +20,97 @@ user_collection = db["Users"]
 project_collection = db["Projects"]
 authorized_collection = db["Authorized Users"]
 
+
 inventory = {}
 joined = -1
-
-# #jwt = JWTManager(app)
-# @app.after_request
-# def refresh_expiring_jwts(response):
-#     try:
-#         exp_timestamp = get_jwt()["exp"]
-#         now = datetime.now(timezone.utc)
-#         target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-#         if target_timestamp > exp_timestamp:
-#             access_token = create_access_token(identity=get_jwt_identity())
-#             data = response.get_json()
-#             if type(data) is dict:
-#                 data["access_token"] = access_token 
-#                 response.data = json.dumps(data)
-#         return response
-#     except (RuntimeError, KeyError):
-#         # Case where there is not a valid JWT. Just return the original respone
-#         return response
-
-
-# @app.route('/projects')
-# #@jwt_required()
-# def my_profile():
-#     response_body = {
-#         "name": "JimboandFriends",
-#         "about" :"sup"
-#     }
-
-#     return response_body
-    
-
-# @app.route('/token', methods=["POST"])
-# def create_token():
-#     email = request.json.get("email", None)
-#     password = request.json.get("password", None)
-#     if email != "test" or password != "test":
-#         return {"msg": "Wrong email or password"}, 401
-
-#     access_token = create_access_token(identity=email)
-
-#     response = {"access_token":access_token}
-#     return response
-
-
-# @app.route("/logout", methods=["POST"])
-# def logout():
-#     response = jsonify({"msg": "logout successful"})
-#     unset_jwt_cookies(response)
-#     return response
 
 @app.route("/")
 def index():
     print("ERHRERERE")
     return app.send_static_file('index.html')
 
-# @app.errorhandler(404)
-# def not_found(e):
-#     return app.send_static_file('index.html')
 
-@app.route("/api/checkIn/<int:proj_id>/<int:qty>/<int:HWSet>")
-def checkIn_hardware(proj_id, qty, HWSet):
-    if proj_id in inventory:
-        if HWSet == 1:
-            inventory[proj_id][0] += qty
-            if inventory[proj_id][0] >= 100:
-                inventory[proj_id][0] = 100
+@app.route("/api/checkIn/<int:projectID>/<int:hwSet>/<int:qty>", methods=['GET'])
+@cross_origin()
+def checkIn_Hardware(projectID: int, hwSet: int, qty: int):
+    maxQty = 100
+    setNumber = "hw" + str(hwSet)
+    
+    project = project_collection.find_one({'projectID': projectID})
+
+    if project is not None:
+        currentQty = project[setNumber]
+        
+        if currentQty + qty > maxQty:
+            return jsonify({'error': 'total qty exceeds maxQty'}), 400
         else:
-            inventory[proj_id][1] += qty
-            if inventory[proj_id][1] >= 100:
-                inventory[proj_id][1] = 100
+            doc = project_collection.find_one_and_update(
+                {"projectID" : projectID},
+                {"$set":
+                    {setNumber: currentQty + qty}
+                },upsert=True
+            )
+            ret = {
+                "projectID": projectID,
+                "hwSet": hwSet,
+                "qty": currentQty + qty
+            }
     else:
-        if HWSet == 1:
-            inventory[proj_id] = [qty, 0]
-        else:
-            inventory[proj_id] = [0, qty]
-
-    return jsonify({'qty': qty})
+        return jsonify({'error': 'project does not exist'}), 400
 
 
+    return jsonify(ret)
 
-@app.route("/api/checkOut/<int:proj_id>/<int:qty>/<int:HWSet>")
-def checkOut_hardware(proj_id, qty, HWSet):
-    if proj_id in inventory:
-        if HWSet == 1:
-            inventory[proj_id][0] -= qty
-            if inventory[proj_id][0] <= 0:
-                inventory[proj_id][0] = 0
-        else:
-            inventory[proj_id][1] -= qty
-            if inventory[proj_id][1] <= 0:
-                inventory[proj_id][1] = 0
+
+@app.route("/api/checkOut/<int:projectID>/<int:hwSet>/<int:qty>")
+@cross_origin()
+def checkOut_hardWare(projectID: int, hwSet: int, qty: int):
+    setNumber = "hw" + str(hwSet)
+    project = project_collection.find_one({'projectID': projectID})
+
+    if project is not None:
+        currentQty = project[setNumber]        
+        if qty > currentQty:
+            qty = currentQty
+        doc = project_collection.find_one_and_update(
+            {"projectID" : projectID},
+            {"$set":
+                {setNumber: currentQty - qty}
+            },upsert=True
+        )
+        ret = {
+            'projectID': projectID,
+            'hwSet': setNumber,
+            'qty': currentQty - qty
+        }
+        return jsonify(ret)
+
+    return jsonify({'error': 'project does not exist'}), 400
+
+
+@app.route("/api/join/<int:projectId>")
+@cross_origin()
+def joinProject(projectId: int):
+    global current_project
+    if current_project != -1:
+        return jsonify({'error': 'already in a group'}), 400
     else:
-        return jsonify({'error': 'Project ID does not exist'})
-
-    return jsonify({'qty': qty})
-
-@app.route("/api/joinProject/<int:proj_id>")
-def joinProject(proj_id):
-    global joined
-    if joined == -1:
-        joined = proj_id
-    else:
-        return jsonify({'status': 0, 'msg': 'Already joined a project!'})
-    return jsonify({'status': 1, 'msg': f'Joined project {proj_id}'})
+        current_project = projectId
+    ret = {
+        'projectId': current_project
+    }
+    return jsonify(ret)
 
 
+@app.route("/api/leave/<int:projectId>")
+@cross_origin()
+def leaveProject(projectId: int):
+    global current_project
+    current_project = -1
+    ret = {
+        'projectId': projectId
+    }
+    return jsonify(ret)
 
 
 @app.route('/api/login/<username>/<password>/<userID>', methods = ['GET','POST'])
@@ -161,13 +145,15 @@ def signUp(userName, password, userID):
     return jsonify({'msg': "Signup failed"})
 
 
-@app.route('/api/newProject/<projectName>/<projectID>/<description>/<authorized>', methods = ['GET','POST'])
+@app.route('/api/newProject/<projectName>/<int:projectID>/<description>/<authorized>', methods = ['POST'])
 def newProject(projectName, projectID, description, authorized):
     project = {
         'projectName': projectName,
         'projectID': projectID,
         'description': description,
-        'authorized': authorized
+        'authorized': authorized,
+        'hw1' : 0,
+        'hw2' : 0
     }
     
     if project_collection.find_one({'projectID': projectID}):
@@ -183,6 +169,16 @@ def deleteProject(projectName, projectID):
     if project_collection.find_one({'projectID': projectID}):
         project_collection.delete_one({'projectID': projectID})
     return jsonify({'msg': "Project \"" + projectName + "\" deleted", 'new': True})
+
+
+@app.route('/api/projectsList', methods = ['GET'])
+def projectsList():
+
+    cursor = project_collection.find()
+    list_cur = list(cursor)
+    json_data = json.dumps(list_cur, default=str)
+    return json_data
+   
 
 
 if __name__ == '__main__':
